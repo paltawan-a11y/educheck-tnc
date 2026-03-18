f13fb5a06052fe00e265f8c6844f3ee6f190b63b
paltawan <paltawan.official@gmail.com>
2026-03-18 15:22:37 +0700

    Fix Google Redirect URI

--- api/index.ts
+++ api/index.ts
@@ -50,7 +50,7 @@ function writeClassrooms(classrooms: any[]) {
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  process.env.GOOGLE_REDIRECT_URL
+  process.env.GOOGLE_REDIRECT_URI
 );
 
 async function getEmailFromToken(authHeader: string | undefined) {
